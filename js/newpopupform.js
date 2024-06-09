6021bc975d145016a24423721e3c1197df01aeeb
Afolabi Emmanuel <79112781+Justjackson-999@users.noreply.github.com>
2024-06-09 10:49:12 -0400

    Add files via upload

---
+++ js/newpopupform.js
@@ -0,0 +1,62 @@
+//check if js file is connected
+// alert('js is working');
+
+//OPENFORM
+function openForm1() {
+  document.getElementById("myForm1").style.display = "block";
+}
+
+function closeForm1() {
+  document.getElementById("myForm1").style.display = "none";
+}
+
+//OPENFORM2
+function openForm2() {
+  document.getElementById("myForm2").style.display = "block";
+}
+
+function closeForm2() {
+  document.getElementById("myForm2").style.display = "none";
+}
+
+//OPENFORM3
+function openForm3() {
+  document.getElementById("myForm3").style.display = "block";
+}
+
+function closeForm3() {
+  document.getElementById("myForm3").style.display = "none";
+}
+
+//OPENFORM4
+function openForm4() {
+  document.getElementById("myForm4").style.display = "block";
+}
+
+function closeForm4() {
+  document.getElementById("myForm4").style.display = "none";
+}
+
+//OPENFORM5
+function openForm5() {
+  document.getElementById("myForm5").style.display = "block";
+}
+
+function closeForm5() {
+  document.getElementById("myForm5").style.display = "none";
+}
+
+//OPENFORM6
+function openForm6() {
+  document.getElementById("myForm6").style.display = "block";
+}
+
+function closeForm6() {
+  document.getElementById("myForm6").style.display = "none";
+}
+
+//MAILING FORMAT
+
+function websitedesignmail() {
+  window.location.href = "mailto:info@uzorsydney.tech?subject=Enquiries On Website Design(uzorsydney.tech)";
+}
